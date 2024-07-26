611ade3136caf785d6fa7ad85d0bd7f4df176205
AthenasCode <emoore5203@gmail.com>
2024-07-26 10:25:54 +0800

    added about page routing and global layout

--- codesigner/src/app/layout.tsx
+++ codesigner/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
+import styles from "./layout.module.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +19,20 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <header className={styles.header}>
+          <h1 className={styles.title}>acpd</h1>
+          <nav className={styles.nav}>
+            <ul>
+              <li>
+                <Link href={"/about"}>about</Link>
+                <Link href={"/"}>playground</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        {children}
+      </body>
     </html>
   );
 }
